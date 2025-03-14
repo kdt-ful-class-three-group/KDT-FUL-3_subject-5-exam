4cb41f5423bd846ae7f82ecfbdb54cd32ebe5481
kimyohun <yohunkim0@gmail.com>
2025-03-14 15:59:20 +0900

    Img, Splash Art CDN 구현 및 표현

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,5 @@
 let championData = [];
+
 function dataHandle() {
 const xhr = new XMLHttpRequest();
 // console.dir(xhr);
@@ -7,37 +8,31 @@ xhr.send();
 xhr.addEventListener('load', () => {
   if(xhr.status === 200);{
   let data = JSON.parse(xhr.responseText);
-  // console.dir(data);
-  // console.log(Object.values(data.data));
+  
+  // 
   let apiData = Object.values(data.data)
-  apiData.forEach((element, i) => {
-    // console.log(element.name);
+  console.log(Object.keys(data.data));
+  // apiData에서 img 이름 가져오기 위함  
+  let apiImg = [];
+  let apiSplash =[]
+  for(let i = 0 ; i < apiData.length ; i++){
+    apiImg.push(apiData[i].image)
+    apiSplash.push(Object.keys(data.data)[i])
+  }
+  
+  console.log(apiData[0].image)
+
+  apiData.forEach((element, index) => {
     let championName = element.name
     championData.push(championName);
     const ul = document.getElementById('champList');
-    ul.innerHTML+=`<li>${championData[i]}</li>`
-    // console.log(championName);
+    ul.innerHTML+=`<div>
+    <img src = https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${apiImg[index].full}>
+    <img src = https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${apiSplash[index]}_0.jpg>
+    <p>${championData[index]}</p></div>`
   });
-  // console.log(xhr.responseText);
 }
-// const rootDiv = document.getElementById('root'); // HTML에 id가 root인 div가 있어야 합니다.
-// console.log(root);
-// championData.forEach((element, i) => {
-// });
-// const ul = document.createElement('ul');
-// ul.appendChild(root);
-    // rootDiv.innerHTML = championData.join('<br>'); // 챔피언 이름을 줄바꿈으로 표시
 
-
-
-    // championData.forEach((element) => {
-    //   const li = document.createElement('li')
-    
-    //   li.textContent = element
-    
-    
-    //   li.appendChild(ul);
-    // });
 }
 
 );
@@ -45,12 +40,3 @@ xhr.addEventListener('load', () => {
 console.log(championData);
 }
 dataHandle();
-
-// championData.forEach((element) => {
-//   const li = document.createElement('li')
-
-//   li.textContent = element
-
-
-//   li.appendChild(ul);
-// });
