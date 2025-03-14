9cd1ec03c213dfbd2cf3b3c37d17671a3b2872fe
김민지 <qniomo00@gmail.com>
2025-03-14 11:09:45 +0900

    2팀 banPick 클릭 이벤트 시도 > 주석으로 정리

---
+++ server/services/banPickService.js
@@ -0,0 +1,10 @@
+import { CHAMPOBJ } from "../../client/src/obj/CHAMPOBJ_hyunjoon.js";
+
+//banPickData
+//{blue : {ban:[], pick:[]},red:{ban:[], pick:[]}}
+console.log(CHAMPOBJ)
+
+//[ ] 클릭하면 배열에 text 넣기
+//article 가져오기
+//클릭 이벤트 : 배열(ban)에 text 넣기
+//console로 출력
