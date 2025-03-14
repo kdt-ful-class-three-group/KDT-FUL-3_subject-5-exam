3abad47feca7aea2fb1b4d359680488b270f5d30
김민지 <qniomo00@gmail.com>
2025-03-14 12:24:24 +0900

    [ ]todo 작성

--- client/src/components/banPickService.js
+++ client/src/components/banPickService.js
@@ -1,6 +1,12 @@
+console.log("연결됬니?")
 // import { CHAMPOBJ } from "./obj/CHAMPOBJ_hyunjoon.js";
 
-console.log("연결됬니?")
 
 //[ ] section#list > div 선택하면 id,name 가져와서 배열에 담기
-//[ ] ban[] 에 담은 데이터 div#color > div 
+//[ ] ban[] 에 담은 데이터 div#color > div에 넣기
+
+//[ ] 중복 안됨
+
+//[ ] 선택 안하면?
+
+//[ ] 최대 10번
