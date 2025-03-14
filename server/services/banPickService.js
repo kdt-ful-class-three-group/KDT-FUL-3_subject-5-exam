22f717b162eda1d9ca0ecdc7ee00077316cc7a9b
최현준 <choijjunii1@gmail.com>
2025-03-14 11:45:22 +0900

    클릭이벤트, 배열담기

--- server/services/banPickService.js
+++ server/services/banPickService.js
@@ -3,14 +3,9 @@ import { CHAMPOBJ } from "../../client/src/obj/CHAMPOBJ_hyunjoon.js";
 let data = CHAMPOBJ.banPickData;
 
 
-
-
-
-
-
-
-
 console.log(data.blue.ban); // []
 console.log(data.blue.pick); // []
 console.log(data.red.ban); // []
 console.log(data.red.pick); // []
+
+
