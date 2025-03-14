fdaa0bc86b1a261836c0ef632ba5ca71bcd8796f
김재승 <jaes@gmail.com>
2025-03-14 11:43:49 +0900

    서버구축완성

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// __dirname을 ES 모듈에서 사용하기 위한 설정
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -11,25 +16,10 @@ app.use(express.json());
 
 app.use("/public", express.static("public"));
 
-
-
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
-
-
-
-
-
 
 app.listen(process.env.PORT, () => {
   console.log(`loading... http://localhost:${process.env.PORT}`);
 });
-
-
-
-
-
-
-
-
