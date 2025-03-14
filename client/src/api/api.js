1f7fdfc0cbc5369fd295c10c89ad6aff60b37e54
JeonSunil <jeonsunil1225@gmail.com>
2025-03-14 14:49:42 +0900

    1팀 xhr 사용 확정 및 li로 챔피언 불러오기

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,22 +1,56 @@
-let data = function(){
-    fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json")
-        .then(response => response.json())
-        .then(data => {
-          // 객체로 된 데이터 value로 받아와서 data 객체로 진입
-          let dataGet = Object.values(data.data)
-          // 한글 이름 저장소
-          let koreanData = [];
-          dataGet.forEach((element) =>{
-            // 한글 이름 순서대로 배열에 삽입
-            koreanData.push(element.name);
-          })
-          console.log(koreanData);
-          for(let i = 0 ; i < koreanData.length ; i++){
-              let dataSet = document.createElement('p');
-              dataSet.textContent = koreanData[i];
-              document.body.appendChild(dataSet)
-          }
-        })
+let championData = [];
+function dataHandle() {
+const xhr = new XMLHttpRequest();
+// console.dir(xhr);
+xhr.open("GET", 'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json');
+xhr.send();
+xhr.addEventListener('load', () => {
+  if(xhr.status === 200);{
+  let data = JSON.parse(xhr.responseText);
+  // console.dir(data);
+  // console.log(Object.values(data.data));
+  let apiData = Object.values(data.data)
+  apiData.forEach((element, i) => {
+    // console.log(element.name);
+    let championName = element.name
+    championData.push(championName);
+    const ul = document.getElementById('champList');
+    ul.innerHTML+=`<li>${championData[i]}</li>`
+    // console.log(championName);
+  });
+  // console.log(xhr.responseText);
+}
+// const rootDiv = document.getElementById('root'); // HTML에 id가 root인 div가 있어야 합니다.
+// console.log(root);
+// championData.forEach((element, i) => {
+// });
+// const ul = document.createElement('ul');
+// ul.appendChild(root);
+    // rootDiv.innerHTML = championData.join('<br>'); // 챔피언 이름을 줄바꿈으로 표시
+
+
+
+    // championData.forEach((element) => {
+    //   const li = document.createElement('li')
+    
+    //   li.textContent = element
+    
+    
+    //   li.appendChild(ul);
+    // });
 }
 
-data();
+);
+
+console.log(championData);
+}
+dataHandle();
+
+// championData.forEach((element) => {
+//   const li = document.createElement('li')
+
+//   li.textContent = element
+
+
+//   li.appendChild(ul);
+// });
