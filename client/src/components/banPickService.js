4dbf660b3a33f6303961c1a5840dad85df06dac0
김민지 <qniomo00@gmail.com>
2025-03-14 15:36:34 +0900

    2팀 주석으로 설명

--- client/src/components/banPickService.js
+++ client/src/components/banPickService.js
@@ -2,8 +2,13 @@ console.log("연결")
 import { CHAMPOBJ } from "../obj/CHAMPOBJ_hyunjoon.js"
 console.log('obj확인',CHAMPOBJ.banPickData)
 
+
 //클릭 횟수, 배열에 담길 순서를 결정할 변수
-// let clickCount = 0;
+let count = {
+    blue:0,
+    red:0
+};
+
 // let finallCount = 10;
 let clickData=[];
 let finalClick = [];
@@ -25,31 +30,34 @@ console.log(list)
 //숫자 사용을 위해 for문 사용
 
 Array.from(list).forEach(div=>{
+  //div 클릭 이벤트
   div.addEventListener('click',()=>{
-    // console.log('1',clickData)
+    //click한 div의 속성값 배열에 넣기
     clickData.push({id : div.getAttribute('id'), name : div.getAttribute('name')})
-
+    //마지막 요소만 남기기
     if(clickData.length>1){
       clickData.shift()
-      console.log('2',clickData)
+      console.log('click',clickData)
     }
   })
 })
 //버튼
 const choiceBtn = document.getElementById('choice')
+//버튼 클릭 이벤트
 choiceBtn.addEventListener('click',()=>{
+  //div를 클릭했을 때 선택버튼 누르면 최종 배열에 추가 + 클릭데이터 비우기
   if(clickData[0]){
     finalClick.push(clickData[0])
     clickData = []
   }else {
+    //div를 클릭하지 않고 그냥 넘어갔을 경우
     finalClick.push({id:'ban',name:'ban'})
   }
+  //console로 확인
   console.log('last',finalClick)
 
 })
 
-
-// console.log(clickData)
 
 
 
@@ -60,23 +68,3 @@ choiceBtn.addEventListener('click',()=>{
 //[ ] 클릭하면 담기, 선택안하면 걍 안한 거 : null, "noBan"
 
 //[ ] 최대 10번
- const CHAMPOBJ = {
-      banPickData: {
-        blue: { ban: [], pick: [] },
-        red: { ban: [], pick: [] }
-      }
-    }
-
-
-
-//     const blueBtn = document.querySelector('#blueBtn');
-//     const redBtn = document.querySelector('#redBtn');
-
-//     blueBtn.addEventListener('click', () => {
-//       CHAMPOBJ.banPickData.blue.ban.push();
-//       console.log(CHAMPOBJ.banPickData.blue.ban);
-//     });
-//     redBtn.addEventListener('click', () => {
-//       CHAMPOBJ.banPickData.blue.ban.push();
-//       console.log(CHAMPOBJ.banPickData.blue.ban);
-//     });
