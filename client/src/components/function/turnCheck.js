6ab4c3d16762132e04b29ebf3cd77f19d97c364e
김민지 <qniomo00@gmail.com>
2025-03-17 10:38:09 +0900

    2팀 10번 선택한 후 최종 배열에 담는 putDataToArray 함수 작성

--- client/src/components/function/turnCheck.js
+++ client/src/components/function/turnCheck.js
@@ -5,14 +5,15 @@ import { turn } from "./turn.js";
  * 선택버튼을 눌렀을 때 최종선택이 데이터에 담길 수 있게 해주는 함수
  * @param {*} listElement  목록에 해당하는 요소
  * @param {Array} clickLiveData 선택한 목록이 담길 배열
- * @param {Array} fincalClickData 최종 선택이 담길 배열
+ * @param {Array} finalClickData 최종 선택이 담길 배열
+ * @returns {Array} finalClickData 최종 선택이 담긴 배열
  */
-export function turnCheck(listElement,clickLiveData, fincalClickData ){
+export function turnCheck(listElement,clickLiveData, finalClickData ){
   //clickLiveData의 값이 있을 때
   if(clickLiveData[0]){
 
     //최종 배열에 값 넣어주기
-    fincalClickData.push(clickLiveData[0])
+    finalClickData.push(clickLiveData[0])
 
     //최종 값에 해당하는 div를 선택하지 못하도록 함
     Array.from(listElement).filter(div => {
@@ -28,8 +29,10 @@ export function turnCheck(listElement,clickLiveData, fincalClickData ){
   }
   //div선택하지 않고 버튼 눌렀을 때
   else {
-    fincalClickData.push(CHAMPOBJ.noneClick)
+    finalClickData.push(CHAMPOBJ.noneClick)
   }
   
   turn();
+
+  return finalClickData
 }
