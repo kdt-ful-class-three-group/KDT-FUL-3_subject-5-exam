02b4189a46aa0f3c63618f64b7ea21c4528d77c5
김민지 <qniomo00@gmail.com>
2025-03-17 14:58:34 +0900

    주석으로 설명

--- client/src/components/banPickService.js
+++ client/src/components/banPickService.js
@@ -9,20 +9,18 @@ import { nextTurn } from "./function/nextTurn.js";
 
 //클릭 횟수, 배열에 담길 순서를 결정할 변수
 let count = CHAMPOBJ.count
-let clickData=[];
-let finalClick = [];
+
+//중앙 챔피언 리스트 클릭할 때 사용하는 배열
+let clickData=[]; //실시간으로 담기는 배열
+let finalClick = []; //최종 배열
 
 //div 요소
-const blueDiv = document.querySelectorAll('div#blue > div')
-const redDiv = document.querySelectorAll('div#red > div')
-const list = document.getElementById('list').children
+const blueDiv = document.querySelectorAll('div#blue > div') //blue팀 ban리스트 목록
+const redDiv = document.querySelectorAll('div#red > div') //red팀 ban리스트 목록
+const list = document.getElementById('list').children //중앙 챔피언 리스트
 
-//짝수일 때는 blueDiv에
-//홀수 일 때는 redDiv에
-//숫자 사용을 위해 for문 사용
-
+//중앙챔피언리스트 클릭이벤트
 Array.from(list).forEach(div=>{
-
   div.setAttribute('style','cursor:pointer')
 
   //div 클릭 이벤트
@@ -31,14 +29,15 @@ Array.from(list).forEach(div=>{
     //click한 div의 속성값 배열에 넣기
     clickData.push({id : div.getAttribute('id'), name : div.getAttribute('name')})
 
-    //마지막 요소만 남기기
+
+    //중앙 리스트 선택할 때 실시간으로 핸들링 하는 로직 : 마지막 요소만 남기기
     if(clickData.length>1){
       clickData.shift()
       console.log('click',clickData)
     }
-    console.log(count)
+    console.log('count',count)
 
-    //div태그 안에 넣어주기
+    //각 팀 ban리스트 보여주기
     if(count.all%2===0){
       blueDiv[count.blue].textContent = clickData[0].name
     } else {
@@ -46,20 +45,27 @@ Array.from(list).forEach(div=>{
     }
   })
 })
+
+
 //버튼
 const choiceBtn = document.getElementById('choice')
 //버튼 클릭 이벤트
 choiceBtn.addEventListener('click', () => {
+
+  //1-9번 진행
   if (count.total < 9) {
     commitClickData(list, clickData, finalClick);
     nextTurn(count);
   }
+  //10번 : 최종 ban리스트 데이터 (CHAMPOBJ.banpickData.color.ban)에 담기
   else if (count.total === 9) {
     commitClickData(list,clickData, finalClick);
     nextTurn(count);
     saveFinalBanData(CHAMPOBJ, finalClick);
     alert('밴 완료했습니다');
-  } else {
+  } 
+  //11번 눌렀을 때
+  else {
     alert('완료')
   }
   console.log('last', finalClick);
