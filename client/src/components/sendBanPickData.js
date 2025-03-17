8bd71e09a0c286b179492e65761a32809408bf3b
JeonSunil <jeonsunil1225@gmail.com>
2025-03-17 19:39:57 +0900

    1팀 밴픽데이터를 서버로 가져와서Json파일을 만들기

--- client/src/components/sendBanPickData.js
+++ client/src/components/sendBanPickData.js
@@ -1,24 +1,25 @@
-// import { CHAMPOBJ } from "../obj/CHAMPOBJ_hyunjoon.js";
-
-
-// data가 들어오는지 확인
-function sendBanPickData(list){
+// * data가 들어오는지 확인
+// function sendBanPickData(list){
   
-  console.log(list);
+//   console.log(list);
   
-}
-
-// function sendBanPickData() {
-//   fetch("/save", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/javascript",
-//     },
-//     body: CHAMPOBJ.banPickData,
-//   })
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => console.log(`${err} 에러발생`));
 // }
+/**
+ * 
+ * @param {*} list saveFinalBanData.js에서 CHAMPOBJ.banPickData를 가져옴
+ * @description POST로 /save 경로로 app.js에 banPickData를 보내기 위해서 만든 모듈
+ */
+function sendBanPickData(banPickData) {
+  fetch("/save", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(banPickData),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(`${err} 에러발생`));
+}
 
 export {sendBanPickData}
