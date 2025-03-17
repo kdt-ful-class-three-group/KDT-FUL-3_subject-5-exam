a2d1d66c840c602b4b43c4b8e2dba258016b214e
최정민 <bigdragon00@naver.com>
2025-03-17 12:35:59 +0900

    함수분리

--- client/src/components/banPickService.js
+++ client/src/components/banPickService.js
@@ -1,3 +1,5 @@
+import {autoRender} from "./function/renderHandler.js";
+
 console.log("연결")
 import { CHAMPOBJ } from "../obj/CHAMPOBJ_hyunjoon.js"
 console.log('obj확인',CHAMPOBJ.banPickData)
@@ -8,18 +10,13 @@ import { putDataToArray } from "./function/putDataToArray.js";
 
 
 //클릭 횟수, 배열에 담길 순서를 결정할 변수
-let count = {
-    blue:0,
-    red:0,
-    get all() {
-      return this.blue+this.red
-    } ,
-    total: 0
-};
 
+let count = CHAMPOBJ.count
 // let finallCount = 10;
 let clickData=[];
 let finalClick = [];
+
+
 
 //id, name을 객체로 담기
 
@@ -33,6 +30,40 @@ const redDiv = document.querySelectorAll('div#red > div')
 
 const list = document.getElementById('list').children
 
+function deactivateClickedDiv(id) {
+  Array.from(list).forEach(div => {
+    if (div.getAttribute('id') === id) {
+      div.style.pointerEvents = 'none';
+      div.style.color = 'red';
+    }
+  });
+}
+
+function commitClickData() {
+  const data = clickData || { id: 'ban', name: 'ban' };
+  finalClick.push(data);
+
+  if (clickData.length >0) {
+    const id = clickData[0].id;
+    deactivateClickedDiv(clickData.id);
+    clickData = [];
+  }
+}
+
+
+function saveFinalBanData() {
+  CHAMPOBJ.banPickData.blue.ban = finalClick.filter((_, i) => i % 2 === 0);
+  CHAMPOBJ.banPickData.red.ban = finalClick.filter((_, i) => i % 2 !== 0);
+}
+
+function nextTurn() {
+  if (count.total % 2 === 0) {
+    count.blue++;
+  } else {
+    count.red++;
+  }
+  count.total++;
+}
 
 
 console.log(list)
@@ -52,8 +83,6 @@ Array.from(list).forEach(div=>{
     //click한 div의 속성값 배열에 넣기
     clickData.push({id : div.getAttribute('id'), name : div.getAttribute('name')})
 
-
-
     //마지막 요소만 남기기
     if(clickData.length>1){
       clickData.shift()
@@ -72,64 +101,21 @@ Array.from(list).forEach(div=>{
 //버튼
 const choiceBtn = document.getElementById('choice')
 //버튼 클릭 이벤트
-choiceBtn.addEventListener('click',()=>{
-  //div를 클릭했을 때 선택버튼 누르면 최종 배열에 추가 + 클릭데이터 비우기
-  if(count.total<9){
-    turnCheck(list, CHAMPOBJ.clickLiveData,CHAMPOBJ.finalClickData)
-    //함수 사용
-    // if(clickData[0]){
-    //   finalClick.push(clickData[0])
-    //   console.log(clickData[0].id)
-    //   Array.from(list).filter(div => {
-    //     if (div.getAttribute('id') === clickData[0].id) {
-    //       console.log("divtest", div);
-    //       div.style.pointerEvents='none'
-    //       div.style.color='red'
-    //       return
-    //     }
-    //   })
-    //   clickData = []
-    // }else {
-    //   //div를 클릭하지 않고 그냥 넘어갔을 경우
-    //   finalClick.push({id:'ban',name:'ban'})
-    // }
-    // //blue red 번갈아가며 진행
-    // if(count.total%2===0){
-    //   count.blue++
-    // } else {
-    //   count.red++
-    // }
-    // count.total++;
-  } 
-  else if(count.total=9) {
-    turnCheck(list, CHAMPOBJ.clickLiveData,CHAMPOBJ.finalClickData)
-    // if(clickData[0]){
-    //   finalClick.push(clickData[0])
-    //   clickData = []
-    // }else {
-    //   //div를 클릭하지 않고 그냥 넘어갔을 경우
-    //   finalClick.push({id:'ban',name:'ban'})
-    // }
-    // //blue red 번갈아가며 진행
-    // if(count.total%2===0){
-    //   count.blue++
-    // } else {
-    //   count.red++
-    // }
-    // count.total++;
-
-    putDataToArray()
-
-    CHAMPOBJ.banPickData.blue.ban = finalClick.filter((a,i)=>i%2===0)
-    CHAMPOBJ.banPickData.red.ban = finalClick.filter((a,i)=>i%2!==0)
-    console.log('최종',CHAMPOBJ.banPickData)
-    alert('밴 완료했습니다')
+choiceBtn.addEventListener('click', () => {
+  if (count.total < 9) {
+    commitClickData();
+    nextTurn();
   }
-  //11번 - 20번 : pick
-  else if(count.total>10){
-    alert('11번째 입니다.')
+  else if (count.total === 9) {
+    commitClickData();
+    nextTurn();
+    putDataToArray();
+    saveFinalBanData();
+    autoRender();
+    alert('밴 완료했습니다');
   }
-  //console로 확인
-  console.log('last',finalClick)
-
-})
+  else if (count.total > 10) {
+    alert('11번째 입니다.');
+  }
+  console.log('last', finalClick);
+});
