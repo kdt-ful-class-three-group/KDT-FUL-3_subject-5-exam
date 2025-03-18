62448ce7ff7a47703b34fc4b5b2f82f3d966f4a6
김재승 <jaes@gmail.com>
2025-03-18 17:24:33 +0900

    벤픽오류수정

--- client/src/components/banPickService.js
+++ client/src/components/banPickService.js
@@ -93,43 +93,44 @@ banBtn.addEventListener("click", () => {
 
 //pick버튼 클릭
 pickBtn.addEventListener("click", () => {
-  startTimer(banBtn);
   //리스트 선택안하고 pick버튼 눌렀을 때
   if (clickData.length === 0) {
+    // startTimer(banBtn);
     alert("픽을 진행해주세요");
-    return;
-  }
+    // return;
+  } else {
+    startTimer(banBtn);
+    //1-9번
+    if (count.total < 9) {
+      //마지막으로 선택한 요소 담기
+      commitClickData(list, clickData, finalClick);
+      //blue, red 번갈아 진행
+      nextTurn(count);
+    }
+    //10번
+    else if (count.total === 9) {
+      //마지막으로 선택한 요소 담기
+      commitClickData(list, clickData, finalClick);
+      //blue red 번갈아 진행
+      nextTurn(count);
+      //banpickData.color.pick에 데이터 담기
+      saveFinalData(CHAMPOBJ, "pick", finalClick);
 
-  //1-9번
-  if (count.total < 9) {
-    //마지막으로 선택한 요소 담기
-    commitClickData(list, clickData, finalClick);
-    //blue, red 번갈아 진행
-    nextTurn(count);
-  }
-  //10번
-  else if (count.total === 9) {
-    //마지막으로 선택한 요소 담기
-    commitClickData(list, clickData, finalClick);
-    //blue red 번갈아 진행
-    nextTurn(count);
-    //banpickData.color.pick에 데이터 담기
-    saveFinalData(CHAMPOBJ, "pick", finalClick);
+      //pick버튼 안보임
+      //다시하기 버튼 보임
+      pickBtn.classList.toggle("hidden");
+      restartBtn.classList.toggle("hidden");
+      alert("완료했습니다");
 
-    //pick버튼 안보임
-    //다시하기 버튼 보임
-    pickBtn.classList.toggle("hidden");
-    restartBtn.classList.toggle("hidden");
-    alert("완료했습니다");
+      //console로 확인
+      console.log(CHAMPOBJ.banPickData);
+      console.log("last", finalClick);
+      //count에 사용한 변수 리셋
+      resetCount(count);
 
-    //console로 확인
-    console.log(CHAMPOBJ.banPickData);
-    console.log("last", finalClick);
-    //count에 사용한 변수 리셋
-    resetCount(count);
-
-    //20번 다 고르면 타이머 스탑
-    onTimerExpired();
+      //20번 다 고르면 타이머 스탑
+      onTimerExpired();
+    }
   }
 });
 
