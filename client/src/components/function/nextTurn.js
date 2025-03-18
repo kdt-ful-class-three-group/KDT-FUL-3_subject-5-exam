61340f2035c088abc79864bd0d93f830b3c33fd1
윤종환 <yunjonghwan99@gmail.com>
2025-03-18 15:13:12 +0900

    팀3 - 버튼 클릭시 버튼의 색상이 변경되지 않았기에 수정 함

--- client/src/components/function/nextTurn.js
+++ client/src/components/function/nextTurn.js
@@ -5,14 +5,25 @@
 export function nextTurn(count) {
   const blue = document.getElementById('blue');
   const red = document.getElementById('red');
+  const banBtn = document.getElementById('banBtn');
+  const pickBtn = document.getElementById('pickBtn');
+
   if (count.total % 2 === 0) {
     blue.classList.remove('bg-[#004F9D40]');
     red.classList.add('bg-[#FF000040]');
+    banBtn.classList.remove('bg-[#3131FF]');
+    banBtn.classList.add('bg-[#B22222]');
+    pickBtn.classList.remove('bg-[#3131FF]');
+    pickBtn.classList.add('bg-[#B22222]');
     count.blue++;
   } else {
     count.red++;
     red.classList.remove('bg-[#FF000040]');
     blue.classList.add('bg-[#004F9D40]');
+    banBtn.classList.remove('bg-[#B22222]');
+    banBtn.classList.add('bg-[#3131FF]');
+    pickBtn.classList.remove('bg-[#B22222]');
+    pickBtn.classList.add('bg-[#3131FF]');
   }
   count.total++;
 }
