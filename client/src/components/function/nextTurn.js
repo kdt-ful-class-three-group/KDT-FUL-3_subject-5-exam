0343052f8ec173aeb3ab5ec2bf3dbebe299afedc
최현준 <choijjunii1@gmail.com>
2025-03-18 14:53:18 +0900

    2팀 턴 배경수정

--- client/src/components/function/nextTurn.js
+++ client/src/components/function/nextTurn.js
@@ -3,10 +3,18 @@
  * @param {object} count blue, red, total이 들어있는 객체
  */
 export function nextTurn(count) {
+  const blue = document.getElementById('blue');
+  const red = document.getElementById('red');
+
   if (count.total % 2 === 0) {
     count.blue++;
+    blue.style.backgroundColor = "";
+    red.style.backgroundColor = "#ff000082";
   } else {
     count.red++;
+    red.style.backgroundColor = "";
+    blue.style.backgroundColor = "#0080ff82";
+
   }
   count.total++;
 }
