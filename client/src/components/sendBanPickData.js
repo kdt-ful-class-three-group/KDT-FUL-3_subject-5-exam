505f73aa2caa82ed900344526a36c2ad96f35cf7
JeonSunil <jeonsunil1225@gmail.com>
2025-03-18 09:55:32 +0900

    1팀 json파일 생성 및 추가 기능

---
+++ client/src/components/sendBanPickData.js
@@ -0,0 +1,25 @@
+// * data가 들어오는지 확인
+// function sendBanPickData(list){
+  
+//   console.log(list);
+  
+// }
+/**
+ * 
+ * @param {*} list saveFinalBanData.js에서 CHAMPOBJ.banPickData를 가져옴
+ * @description POST로 /save 경로로 app.js에 banPickData를 보내기 위해서 만든 모듈
+ */
+function sendBanPickData(banPickData) {
+  fetch("/save", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(banPickData),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(`${err} 에러발생`));
+}
+
+export {sendBanPickData}
